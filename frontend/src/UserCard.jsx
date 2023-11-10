a353ab7705b6afdb01bd41755fd60c963f455ee1
Ayush Srivastava <asish.ayush.sri@gmail.com>
2023-11-10 16:53:27 +0530

    done with profile card

--- frontend/src/UserCard.jsx
+++ frontend/src/UserCard.jsx
@@ -1,4 +1,8 @@
 import "./index.css";
+import {
+    AiOutlineTwitter,
+    AiFillInstagram,
+  } from "react-icons/ai";
 const UserCard = () => {
     const url="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png";
     return <div class="cardStyle">
@@ -8,10 +12,31 @@ const UserCard = () => {
         <div id="title">Ayush Srivastava</div>
         <div id ="subtitle">Sriyush</div>
         <div id="stats">
+        <div class="posts">
+            <div class="stat-num">5</div>
+            <div class="stat-type">Posts</div>
+        </div >
+        <div class="followers">
             <div class="stat-num">1000</div>
-            <div class="stat-type">follower</div>
+            <div class="stat-type">followers</div>
         </div>
-        <div id ="actions">Pdf file</div>
+        <div class="following">
+            <div class="stat-num">100 k</div>
+            <div class="stat-type">following</div>
+        </div>
+        </div>
+        <div id ="actions">A guy who is just a Guy</div>
+        <div style={{ display: "flex", alignItems: "center" }}>
+                <div style={{ flex: 1, backgroundColor: "#6F38C5", height: "3px" }} />
+            
+                <p style={{ margin: "0 10px" ,fontSize: "12px"}}>Other Social Handles</p>
+            
+                <div style={{ flex: 1, backgroundColor: "#6F38C5", height: "3px" }} />
+        </div>
+        <div style={{ display: "flex", justifyContent: "center", marginTop: "10px" }}>
+        <AiOutlineTwitter style={{ fontSize: "20px", margin: "0 10px" }} />
+        <AiFillInstagram style={{ fontSize: "20px", margin: "0 10px" }} />
+      </div>
     </div>
 }
 export default UserCard;
