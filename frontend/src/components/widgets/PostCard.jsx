21934ecd4880c0819783bc1abab491a3323fa700
aayush20jk <aayushkumar.jha2001@gmail.com>
2023-11-21 12:11:26 +0530

    fixed width issue

--- frontend/src/components/widgets/PostCard.jsx
+++ frontend/src/components/widgets/PostCard.jsx
@@ -72,7 +72,7 @@ const PostCard = ({data}) => {
           <MoreVertIcon onClick={handleMenuClick} />
           </div>
         </div>
-        <div className="text-sm1 footer2" style={{maxWidth:'400px'}}>
+        <div className="text-sm1 footer2" style={{maxWidth:'500px'}}>
             {data.thought} <span style={{ color: 'blue', marginLeft: "10px"}}></span>
           </div>
         <div className="footer1">
